93eed69f558140f0b609448c33344374ac534eb6
YuSan <ysoga19@gmail.com>
2021-02-20 23:10:18 +0900

    下書き的なのの追加

---
+++ components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="text-white body-font bg-secondary">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <a class="flex title-font font-medium items-center text-primary mb-4 md:mb-0">
+        <span class="ml-3 text-xl">Code for Yay!</span>
+      </a>
+      <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+        <a class="mr-5 hover:text-gray-400">First Link</a>
+        <a class="mr-5 hover:text-gray-400">Second Link</a>
+        <a class="mr-5 hover:text-gray-400">Third Link</a>
+        <a class="mr-5 hover:text-gray-400">Fourth Link</a>
+      </nav>
+      <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+        Button
+      </button>
+    </div>
+  </header>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,26 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        website
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <Navbar />
+    <section class="text-gray-300 body-font bg-secondary">
+      <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+        <div class="text-center lg:w-2/3 w-full">
+          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-primary">
+            We make something fun, by the fun, for the fun.
+          </h1>
+          <p class="mb-8 leading-relaxed">
+            Code for Yay は楽しいものを楽しむために楽しく作るクリエイターグループです。
+          </p>
+          <div class="flex justify-center">
+            <button class="inline-flex text-white bg-primary border-0 py-2 px-6 focus:outline-none hover:bg-shamrock-500 rounded text-lg">
+              Button
+            </button>
+            <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+              Button
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
@@ -34,47 +31,4 @@ export default Vue.extend({})
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
